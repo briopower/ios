c39f66cf199d499fe37bf96e588cc52d7c370f84
Shubhanshu Tibrewal <shubhanshu.tibrewal@finoit.co.in>
2016-12-27 10:44:06 +0530

    Filter section Updated

--- ActionableHealth/ActionableHealth/Source/Modules/TabBarModule/HomeModule/FiltersViewController/FilterByObject/FilterByObject.swift
+++ ActionableHealth/ActionableHealth/Source/Modules/TabBarModule/HomeModule/FiltersViewController/FilterByObject/FilterByObject.swift
@@ -10,77 +10,33 @@ import UIKit
 
 class FilterByObject: NSObject {
     //MARK:- Variables
-    var properties = NSMutableArray()
+    var subProperties = NSMutableArray()
 
     //MARK:- Additional methods
 
-    func getSortDescriptors() -> [NSSortDescriptor] {
-        let sortDescriptor_name = NSSortDescriptor(key: "name", ascending: true)
-        let sortDescriptor_isSelected = NSSortDescriptor(key: "isSelected", ascending: false)
-        return [sortDescriptor_isSelected, sortDescriptor_name]
-    }
-    func sortProperties() {
-        sortSubProperties()
-        properties.sortUsingDescriptors(getSortDescriptors())
-    }
-
-    func sortSubProperties() {
-        for obj in properties {
-            if let prop = obj as? Properties {
-                prop.sortSubProperties()
-            }
-        }
-    }
-
-    func selectProperty(property:Properties) {
-        for obj in properties {
-            if let prop = obj as? Properties {
-                if prop == property {
-                    prop.isSelected = true
-                }else{
-                    prop.isSelected = false
-                }
-            }
-        }
-    }
-
     func selectSubProperty(subProperty:SubProperties) {
-        if subProperty.isSelected {
-            subProperty.isSelected = false
-            subProperty.parentProperty?.selectedItemsCount -= 1
-            if subProperty.parentProperty?.selectedItemsCount < 0 {
-                subProperty.parentProperty?.selectedItemsCount = 0
-            }
-        }else{
-            subProperty.isSelected = true
-            subProperty.parentProperty?.selectedItemsCount += 1
+        for obj in subProperties {
+            if let prop = obj as? SubProperties {
+                if prop == subProperty {
+                    prop.isSelected = !(prop.isSelected)
+                }            }
         }
     }
 
     func deselectAllSubProperties() {
-        for obj in properties {
-            if let prop = obj as? Properties {
-                prop.selectedItemsCount = 0
-                for temp in prop.subProperties {
-                    if let subProp = temp as? SubProperties {
-                        subProp.isSelected = false
-                    }
-                }
+        for obj in subProperties {
+            if let prop = obj as? SubProperties {
+              prop.isSelected = false
             }
         }
     }
     
     func getAllSelectedSubproperties() -> NSArray{
         var arr = [String]()
-        for obj in properties {
-            if let prop = obj as? Properties {
-                for temp in prop.subProperties {
-                    if let subProp = temp as? SubProperties {
-                        if subProp.isSelected == true{
-                            arr.append(subProp.name)
-                            
-                        }
-                    }
+        for obj in subProperties {
+            if let prop = obj as? SubProperties {
+                if prop.isSelected == true{
+                    arr.append(prop.name)
                 }
             }
         }
@@ -88,31 +44,9 @@ class FilterByObject: NSObject {
     }
 }
 
-
-//MARK:- ------------------
-class Properties: NSObject {
-    //MARK:- Variables
-    var name = ""
-    var selectedItemsCount = 0
-    var isSelected = false
-    var subProperties = NSMutableArray()
-
-    //MARK:- Additional methods
-    func getSortDescriptors() -> [NSSortDescriptor] {
-        let sortDescriptor_name = NSSortDescriptor(key: "name", ascending: true)
-        let sortDescriptor_isSelected = NSSortDescriptor(key: "isSelected", ascending: false)
-        return [sortDescriptor_isSelected, sortDescriptor_name]
-    }
-    func sortSubProperties() {
-        subProperties.sortUsingDescriptors(getSortDescriptors())
-    }
-}
-
-
 //MARK:- ------------------
 class SubProperties: NSObject {
     //MARK:- Variables
     var name = ""
     var isSelected = false
-    var parentProperty:Properties?
 }
--- ActionableHealth/ActionableHealth/Source/Modules/TabBarModule/HomeModule/FiltersViewController/FiltersViewController.swift
+++ ActionableHealth/ActionableHealth/Source/Modules/TabBarModule/HomeModule/FiltersViewController/FiltersViewController.swift
@@ -11,7 +11,6 @@ import UIKit
 class FiltersViewController: CommonViewController {
 
     //MARK:- Outlets
-    @IBOutlet weak var propertiesTblView: PropertiesTableView!
     @IBOutlet weak var subPropertiesTblView: SubPropertiesTableView!
     
     //MARK:- Variables
@@ -34,104 +33,46 @@ class FiltersViewController: CommonViewController {
 extension FiltersViewController{
     func setupView() {
         if filterObj == nil {
-            filterObj = getFilterByObject()
+            filterObj = getFilterObject()
         }
-        
-        filterObj.sortProperties()
-        if let prop = filterObj.properties.firstObject as? Properties {
-            prop.isSelected = true
-            subPropertiesTblView.dataArray = prop.subProperties
-            for obj in prop.subProperties {
-                if let localobj = obj as? SubProperties{
-                    for names in selectedFilters {
-                        if localobj.name == String(names){
-                            localobj.isSelected = true
-                            prop.selectedItemsCount = prop.selectedItemsCount+1
-                        }
+        for obj in filterObj.subProperties {
+            if let localobj = obj as? SubProperties{
+                for names in selectedFilters {
+                    if localobj.name == String(names){
+                        localobj.isSelected = true
                     }
                 }
             }
         }
-
-        propertiesTblView.propertiesTableViewDelegate = self
-        propertiesTblView.dataArray = filterObj.properties
-        propertiesTblView.setupTableView()
-
         subPropertiesTblView.subPropertiesTableViewDelegate = self
+        subPropertiesTblView.dataArray = filterObj.subProperties
         subPropertiesTblView.setupTableView()
     }
 
-    func getFilterByObject() -> FilterByObject {
+    func getFilterObject() -> FilterByObject {
+        
         let filterObj = FilterByObject()
-
-//        let prop1 = Properties()
-//        prop1.name = "Category"
-//
-//        let subProperty1 = SubProperties()
-//        subProperty1.name = "Exercise"
-//        subProperty1.parentProperty = prop1
-//        prop1.subProperties.addObject(subProperty1)
-//
-//        let subProperty2 = SubProperties()
-//        subProperty2.name = "Fitness"
-//        subProperty2.parentProperty = prop1
-//        prop1.subProperties.addObject(subProperty2)
-//
-//        let subProperty3 = SubProperties()
-//        subProperty3.name = "Diet"
-//        subProperty3.parentProperty = prop1
-//        prop1.subProperties.addObject(subProperty3)
-//
-//        let subProperty4 = SubProperties()
-//        subProperty4.name = "Food"
-//        subProperty4.parentProperty = prop1
-//        prop1.subProperties.addObject(subProperty4)
-//
-//        filterObj.properties.addObject(prop1)
-
-
-        let prop2 = Properties()
-        prop2.name = "Type"
-
-        let subProperty12 = SubProperties()
-        subProperty12.name = "Yoga"
-        subProperty12.parentProperty = prop2
-        prop2.subProperties.addObject(subProperty12)
-
-        let subProperty22 = SubProperties()
-        subProperty22.name = "Training"
-        subProperty22.parentProperty = prop2
-        prop2.subProperties.addObject(subProperty22)
-
-        let subProperty32 = SubProperties()
-        subProperty32.name = "Aerobics"
-        subProperty32.parentProperty = prop2
-        prop2.subProperties.addObject(subProperty32)
-
-        let subProperty42 = SubProperties()
-        subProperty42.name = "Karate"
-        subProperty42.parentProperty = prop2
-        prop2.subProperties.addObject(subProperty42)
-
-        let subProperty52 = SubProperties()
-        subProperty52.name = "HIIT"
-        subProperty52.parentProperty = prop2
-        prop2.subProperties.addObject(subProperty52)
-
-        filterObj.properties.addObject(prop2)
-
+        
+        let subProperty1 = SubProperties()
+        subProperty1.name = "Yoga"
+        filterObj.subProperties.addObject(subProperty1)
+        
+        let subProperty2 = SubProperties()
+        subProperty2.name = "Training"
+        filterObj.subProperties.addObject(subProperty2)
+        
+        let subProperty3 = SubProperties()
+        subProperty3.name = "Aerobics"
+        filterObj.subProperties.addObject(subProperty3)
+        
+        let subProperty4 = SubProperties()
+        subProperty4.name = "Karate"
+        filterObj.subProperties.addObject(subProperty4)
+        
+        let subProperty5 = SubProperties()
+        subProperty5.name = "HIIT"
+        filterObj.subProperties.addObject(subProperty5)
         return filterObj
-    }
-}
-
-//MARK:- PropertiesTableViewDelegate
-extension FiltersViewController:PropertiesTableViewDelegate{
-    func propertySelected(selectedProperty: Properties, index: Int) {
-        filterObj.selectProperty(selectedProperty)
-        selectedProperty.sortSubProperties()
-        subPropertiesTblView.dataArray = selectedProperty.subProperties
-        propertiesTblView.reloadData()
-        subPropertiesTblView.reloadData()
     }
 }
 
@@ -139,7 +80,6 @@ extension FiltersViewController:PropertiesTableViewDelegate{
 extension FiltersViewController: SubPropertiesTableViewDelagate{
     func subPropertySelected(selectedSubProperty: SubProperties, index: Int) {
         filterObj.selectSubProperty(selectedSubProperty)
-        propertiesTblView.reloadData()
         subPropertiesTblView.reloadData()
     }
 }
@@ -150,7 +90,6 @@ extension FiltersViewController{
     }
     @IBAction func clearButtonActions(sender: UIButton) {
         filterObj.deselectAllSubProperties()
-        propertiesTblView.reloadData()
         subPropertiesTblView.reloadData()
     }
     @IBAction func applyButtonActions(sender: UIButton) {

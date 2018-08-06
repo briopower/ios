f3d79938529c834e4c260910131db9f42b94cfc9
Vaibhav Singla <vaibhav.singla@finoit.co.in>
2018-08-06 19:03:12 +0530

    Blog viewing screen added

---
+++ ActionableHealth/Source/Modules/TabBarModule/TracksModule/BlogViewController/BlogViewController.swift
@@ -0,0 +1,35 @@
+//
+//  BlogViewController.swift
+//  ActionableHealth
+//
+//  Created by Vaibhav Singla on 8/6/18.
+//  Copyright © 2018 Finoit Technologies. All rights reserved.
+//
+
+import UIKit
+
+class BlogViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
